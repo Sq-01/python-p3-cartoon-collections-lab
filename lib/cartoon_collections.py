cbf3d8341c0ac34e6604c72f9453731ff8a85d1d
Sadiq <sadiq.said@student.moringaschool.com>
2023-10-18 11:25:38 +0300

    Assignment completed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f'{i}. {dwarf}')
+    #pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(elements):
+    return [element.capitalize() + '!' for element in elements]
+    #pass
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+    #pass
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheeses:
+            return item
+    return None
+    #pass
